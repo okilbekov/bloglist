7214336c29f97ff1cd33604228fc9c6fbab8e6d3
Mirshokhid <okilbekovmirshokhid@gmail.com>
2023-01-25 04:03:22 -0500

    small fix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const app = require('./app')
 const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${process.env.PORT}`)
 })
