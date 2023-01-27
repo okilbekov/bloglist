31a04614531c2435abacc756adb97ee65624b692
Mirshokhid <okilbekovmirshokhid@gmail.com>
2023-01-27 06:13:26 -0500

    added functionality for deleting and updating blog

--- server/controllers/blogs.js
+++ server/controllers/blogs.js
@@ -19,4 +19,18 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const blog = {
+    ...request.body
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
